f9d601f4694497e33e13fcf96249a7c3beb394f9
rcpastasc <62120664+rcpastasc@users.noreply.github.com>
2021-03-09 23:46:10 -0500

    uno
    uno

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+
+	int cantidadAsientos() {
+		int cantidad = 0;
+		return cantidad;
+	}
+
+	String verificarIntegridad() {
+		String mensaje = "Auto original";
+		return mensaje;
+	}
+}
